18b5daa620a3750bddd4c76eb4b52d7e9a2b5540
gmnithinsai <gmnithinsai2599@gmail.com>
2022-02-09 15:53:58 +0530

    commit

--- telusko/calc/views.py
+++ telusko/calc/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
 def home(request):
-    return HttpResponse('Hello world!')
+    return render(request, 'home.html',
+    {
+    'name':'Nithin',
+    'age' : 21,
+    'role' : 'Junior Software Engineer'})
+
+def add(request):
+    n1 = int(request.POST['num1'])
+    n2 = int(request.POST['num2'])
+    return render(request, 'result.html',{'result':n1+n2})
