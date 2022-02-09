da136023902513a6cd30d430444a44ef437ee498
G M Nithin Sai <gmnithinsai2599@gmail.com>
2022-02-09 10:42:48 +0530

    Update settings.py

--- telusko/telusko/settings.py
+++ telusko/telusko/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mq7!$o8a(orfvw442b4mcrzs6lg_^=hg7jn5(bdrf%pbae*q-@'
+SECRET_KEY = 'django-insecure-mq7!$o8a(orfvw433b4mcrzs6lg_^=hg7jn5(bdrf%pbae*q-@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
