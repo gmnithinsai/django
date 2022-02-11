23773497e8178ed6037f45be6a32947848cb8fca
gmnithinsai <gmnithinsai2599@gmail.com>
2022-02-11 16:04:47 +0530

    commit

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -8,3 +8,15 @@ def index(request):
 
     return render(request, 'index.html', {'dests': dests})
 
+def tirupati(request):
+    return render(request, 'tirupati.html')
+
+def hyderabad(request):
+    return render(request, 'hyderabad.html')
+
+def mumbai(request):
+    return render(request, 'mumbai.html')
+
+def bangalore(request):
+    return render(request, 'bangalore.html')
+
