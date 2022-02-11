b958f6c080d743a4fadad86881297c458f315ee5
gmnithinsai <gmnithinsai2599@gmail.com>
2022-02-11 09:57:08 +0530

    commit

--- telusko/telusko/settings.py
+++ telusko/telusko/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'telusko.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'telusko',
+        'USER': 'postgres',
+        'PASSWORD': 'test',
+        'HOST' : 'localhost'
     }
 }
 
@@ -118,9 +122,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'/static')
+    os.path.join(BASE_DIR,'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
-def home(request):
-    return render(request, 'index.html')
+def index(request):
+    dests = Destination.objects.all()
+    
+
+    return render(request, 'index.html', {'dests': dests})
+
